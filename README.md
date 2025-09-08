7f6cf508b11fc4fa76de01e6faaa54c18dd9ad64
jonahmabry <jonahmabry@gmail.com>
2025-09-08 15:52:47 -0500

    First commit and working main.py

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# grade_tracker
+# Grade Tracker
+
+---
+## How it Works:
+#### 1. It uses a Canvas API to access your canvas account.
+#### 2. It then searches for your classes that you are currently enrolled in (In my case I have it set to Fall 2025)
+#### 3. After that, it finds your "current_grade" for each class and saves it to the Shortcuts folder inside your ICloud Drive
+#### 4. Inside your shortcut app on IOS, you can create a shortcut that sends the grades to your phone as a text message each week
+
+## How to Run it:
+#### 1. Clone the repository:
+   ```bash
+   git clone https://github/jonahmabry/grade_tracker.git
+   cd grade_tracker
+   ```
+
+#### 2. Set up the Python environment:
+```bash
+uv sync
+source .venv/bin/activate
+```
+
+#### 3. Configure environment variables:
+Create a `.env` file with your Canvas API key
+
+Your `.env` file should look like:
+```bash
+CANVAS_TOKEN={your_canvas_api_token}
+```
+
+#### 4. Run `main.py`
---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+from datetime import datetime, timedelta, timezone
+import os
+from dotenv import load_dotenv
+import pytz
+
+load_dotenv()
+
+
+# --- CONFIG ---
+API_TOKEN = os.environ.get("CANVAS_TOKEN")
+BASE_URL = "https://auburn.instructure.com/api/v1"
+DAYS_AHEAD = 7
+LOCAL_TZ = pytz.timezone("America/Chicago")
+
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+
+# --- GETTING ACTIVE COURSES ---
+courses = requests.get(f"{BASE_URL}/courses", headers=headers).json()
+filtered_courses = [course for course in courses if course["enrollment_term_id"] == 4407] # 4407 is the id for Fall 2025
+
+
+# --- FETCHING GRADES AND SAVING THEM TO "grades.txt" IN ICLOUD DRIVE/SHORTCUTS ---
+grades_list = []
+for course in filtered_courses:
+    enrollment_data = requests.get(f"{BASE_URL}/courses/{course["id"]}/enrollments", headers=headers, params={"user_id": "4205893"}).json()
+    print(f"{course["name"]}: {enrollment_data[0]["grades"]["current_score"]}")
+    grades_list.append(f"{course['name']}|{enrollment_data[0]['grades']['current_score']}")
+
+
+# --- SAVING GRADES TO "grades.txt" IN ICLOUD DRIVE/SHORTCUTS ---
+# --- SO THAT I CAN RUN A SHORTCUT THAT TEXTS ME MY GRADES EACH WEEK ---
+icloud_path = os.path.expanduser(
+    "/Users/jonahmabry/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/grades.txt"
+)
+
+with open(icloud_path, "w") as f:
+    for line in grades_list:
+        f.write(line + "\n")
+
+
+print(f"Saved {len(grades_list)} grades to {icloud_path}")
